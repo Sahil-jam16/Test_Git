e323bcb2c22acc55c81b00a88e59cb6c1312269f
Sahil-jam16 <sahiljamadar16@gmail.com>
2024-01-11 21:32:58 +0530

    Made the final Game of random number generator

--- random.py
+++ random.py
@@ -5,5 +5,8 @@ def generate_random_number():
 
 if __name__ == "__main__":
     random_number = generate_random_number()
+    print('The Random Number is: ')
+    print("This will be going to be printed...")
     print("Random number:", random_number)
+
 
