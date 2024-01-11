8e5bd9ac247e9269129041973ad0a4106217a5c6
Sahil-jam16 <sahiljamadar16@gmail.com>
2024-01-11 21:26:52 +0530

    Made small changes to check it

--- mannual.py
+++ mannual.py
@@ -1,8 +1,12 @@
 def sort_dictionary(dictionary):
-    sorted_dict = dict(sorted(dictionary.items()))
+    sorted_dict = dict(sorted(dictionary.items(), key=lambda x: x[0]))
     return sorted_dict
 
 # Example usage
 my_dict = {'b': 2, 'a': 1, 'c': 3}
 sorted_dict = sort_dictionary(my_dict)
+
+print('helloword')
+print("123456789101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960")
+
 print(sorted_dict)
